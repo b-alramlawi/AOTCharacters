1ca53e2804fa882ec7d1d4c2b37733d747873af0
b-alramlawi <b.alramlawi300@gmail.com>
2025-05-29 14:52:54 +0300

    Initial Commit

---
+++ AOTCharacters/AOTCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AOTCharactersApp.swift
+//  AOTCharacters
+//
+//  Created by Bahaa Alramlawi on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AOTCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
